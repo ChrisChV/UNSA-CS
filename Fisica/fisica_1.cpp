345752ec3184c8f47574713629473ef16c1a3669
Chris <cristo2014f@outlook.es>
2016-04-15 16:05:28 -0500

    dasdasd

--- Fisica/fisica_1.cpp
+++ Fisica/fisica_1.cpp
@@ -3,36 +3,37 @@
 
 using namespace std;
 
-float t = 0.001;
-float terminus = 100000;
-float presicion = 0.1;
+typedef float Var;
 
-float funcion(float x){
+Var t = 0.001;
+Var terminus = 100000;
+Var presicion = 0.1;
+
+Var funcion(Var x){
 	return (x*x) + x + 1;	
 }
 
-float derivada_media(float xo, float xf){
-	float dx =  xf - xo;
-	float dy = funcion(xf) - funcion(xo);
-	float Dy = dy / dx;
+Var derivada_media(Var xo, Var xf){
+	Var dx =  xf - xo;
+	Var dy = funcion(xf) - funcion(xo);
+	Var Dy = dy / dx;
 	return Dy;
 }
 
 int main(){
-	float dx = 0.001;
-	float xo;
+	Var dx = 0.001;
+	Var xo;
 	cout<<"Ingrese su Xo ";
 	cin>>xo;
-	float xf = xo + dx;
-	float Dya = derivada_media(xo,xf);
-	float Dy = 0;
+	Var xf = xo + dx;
+	Var Dya = derivada_media(xo,xf);
+	Var Dy = 0;
 	int c = 0;
 	do{
 		dx += t;
 		xf = xo + dx;
 		Dy = derivada_media(xo,xf);
-		c++;
-	}while(abs(Dy-Dya) / Dya <= presicion or c == terminus);
-	if(c == terminus) cout<<"El programa no pudo resolver la derevada"<<endl;
+	}while(abs(Dy-Dya) / abs(Dya) <= presicion);
+	//if(c == terminus) cout<<"El programa no pudo resolver la derevada"<<endl;
 	else cout<<"Dya->"<<Dya<<endl<<"Dy->"<<Dy<<endl<<"Xf->"<<xf<<endl<<"dx->"<<dx<<endl;
 }
