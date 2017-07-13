471567f87bad623724452e46b7022e92d73f436b
Chris <cristo2014f@outlook.es>
2017-07-13 00:18:26 -0500

    steno Funciona OK

---
+++ Proyectos/bin/Genetico/Cromosoma.h
@@ -0,0 +1,66 @@
+#ifndef CROMOSOMA_H
+#define CROMOSOMA_H
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+class Cromosoma{
+	public:
+		Cromosoma();
+		Cromosoma(int size);
+		Cromosoma(int size, char * a1, char * a2);
+		int binaryToDecimal();
+		void print();
+		char * vector;
+		int size;
+};
+
+Cromosoma::Cromosoma(){
+	vector = nullptr;
+	size = 0;
+}
+
+Cromosoma::Cromosoma(int size){
+	this->size = size;
+	int random = 0;
+	string stemp = "";
+	vector = new char[size + 1];
+	for(int i = 0; i < size; i++){
+		random = rand() % 2;
+		stemp = to_string(random);
+		vector[i] = stemp[0];
+	}
+	vector[size] = '\0';
+}
+
+Cromosoma::Cromosoma(int size, char * a1, char * a2){
+	this->size = size;
+	vector = new char[size + 1];
+	int size1 = strlen(a1);
+	memcpy(vector,a1,size1);
+	memcpy(&vector[size1],a2,strlen(a2));
+	vector[size] = '\0';
+}
+
+int Cromosoma::binaryToDecimal(){
+	int res = 0;
+	for(int i = 0; i < size; i++){
+		if(vector[i] == '1') res += pow(2,size - 1 - i);
+	}
+	return res;
+}
+
+
+void Cromosoma::print(){
+	for(int i = 0; i < size; i++){
+		cout<<vector[i]<<" ";
+	}
+}
+
+
+#endif
